7928a8de9c68d424c6fc50285a7ff6c8a61a3e70
BryanGonz <bgon@ucla.edu>
2024-02-24 20:49:23 -0600

    Fully functional game. Still need to POST each time we submit a word (Enter) so that it verifies that the entry is an actual word rather than just random letters. Next step is to clean up redundant code.

--- main.js
+++ main.js
@@ -6,36 +6,48 @@ let words = "";
 let index = 0;
 let gameIsOver = false;
 
+async function letterSearch(){
+    const promise = await fetch(getWord);
+    let processedResponse = await promise.json();
+
+    console.log(words, " is words.");
+    const currentWord = words.split("");
+    let wotd = processedResponse.word.split("");
+    let newlist = [0, 0, 0, 0, 0];
+    console.log(currentWord, " is currentWord.");
+    console.log(wotd, " is word of the day.");
+
+    for (let i = 0; i < currentWord.length; i++) {
+        if(currentWord[i] === wotd[i]){
+            newlist[i] = 1;
+            wotd[i] = "0";
+        }
+    }
+    for (let i = 0; i < currentWord.length; i++) {
+        let position = wotd.indexOf(currentWord[i]);
+        if (position >= 0) {
+            newlist[i] = 2;
+        }
+    }
+
+    console.log(newlist);
+    return newlist;
+}
+
 async function getWOTD(){
     const promise = await fetch(getWord);
     let processedResponse = await promise.json();
-    //processedResponse = JSON.stringify(processedResponse);
-        //adds the backspaces
-    //processedResponse = JSON.parse(processedResponse);
-        //removes backspaces from JSON quotes
 
-    console.log("the current word is " + words);
-    console.log("the valid word is " + processedResponse.word);
-
+    console.log(processedResponse.word, " is the wotd.");
     if (processedResponse.word === words) {
         gameIsOver = true;
-        console.log("gameOver is now "+ gameIsOver);
+        //console.log("gameOver is now "+ gameIsOver);
     }
     else {
-        console.log("else Not the correct word");
+        //console.log("else Not the correct word");
         gameIsOver = false;
     }
 }
-//let objective = await getWOTD();
-//console.log(objective + " is the wotd");
-//async function addNewDoggo() {
-//  const promise = await fetch(DOG_URL);
-//  const processedResponse = await promise.json();
-//  const img = document.createElement("img");
-//  img.src = processedResponse.message;
-//  img.alt = "Cute doggo";
-//  doggos.appendChild(img);
-//}
 
 function isLetter(letter) {
     return /^[a-zA-Z]$/.test(letter);
@@ -50,7 +62,7 @@ function addToScreen(char) {
         screen.children[index].innerText = char;
         index++;
     }
-    console.log(index);
+    //console.log(index);
 
 }
 
@@ -63,11 +75,21 @@ function deleteChar() {
     //console.log(index);
 }
 
-function changeColor(newColor) {
+function changeColor(colorList) {
     const temp = index;
+    let count = 0;
     for(let i = temp - 5; i < temp; i++){
-        screen.children[i].style.background = newColor;
-        console.log("i is " + i);
+        switch(colorList[count]) {
+            case 0:
+                screen.children[i].style.background = "#50575e";
+                break;
+            case 1:
+                screen.children[i].style.background = "#005c12";
+                break;
+            case 2:
+                screen.children[i].style.background = "#bd8600";
+        }
+        count++;
     }
 }
 
@@ -77,63 +99,50 @@ function displayMessage(winloss) {
 }
 
 async function submitWord() {
+    const loadingIcon = document.getElementById("spinny");
+    loadingIcon.classList.toggle("hidden");
     await getWOTD();
+    const colorList = await letterSearch();
+    loadingIcon.classList.toggle("hidden");
+
     //console.log("word is being validated is: " + words);
     if (!gameIsOver) {
         words = "";
-        changeColor("#520015");
-        console.log("before checking loss index is " + index);
+        //console.log("before checking loss index is " + index);
         if (index === MAX){
             displayMessage("You lost!");
         }
     }
     else {
         displayMessage("You won!");
-        changeColor("green");
-        console.log("You won!");
+        //console.log("You won!");
     }
-
-    console.log(gameIsOver + " is the game over state");
+    changeColor(colorList);
+    //console.log(gameIsOver + " is the game over state");
 }
 
 function init() {
     addEventListener("keydown", (event) =>{
-        //console.log(event.key)
-        if(event.key === "Backspace"){
-            deleteChar();
-        }
-        else if(event.key === "Enter") {
-            //console.log("enter key");
-            if(words.length % 5 === 0){
-                submitWord();
+            if (!gameIsOver){
+            //console.log(event.key)
+            if(event.key === "Backspace"){
+                deleteChar();
             }
+            else if(event.key === "Enter") {
+                //console.log("enter key");
+                if(words.length % 5 === 0){
+                    submitWord();
+                }
+            }
+            if (!isLetter(event.key)) {
+                event.preventDefault();
+            }
+            else {
+                addToScreen(event.key);
+            }
+            //console.log(words);
         }
-        if (!isLetter(event.key)) {
-            event.preventDefault();
-        }
-        else {
-            addToScreen(event.key);
-        }
-        console.log(words);
     })
 }
 
 init();
-
-
-//const objString = JSON.stringify(dog);
-// adds slashes for JSON object from a JS object
-
-//const response = JSON.parse(response from server);
-
-//const doggos = document.getElementById("dog-target3");
-//async function addNewDoggo() {
-//  const promise = await fetch(DOG_URL);
-//  const processedResponse = await promise.json();
-//  const img = document.createElement("img");
-//  img.src = processedResponse.message;
-//  img.alt = "Cute doggo";
-//  doggos.appendChild(img);
-//}
-//
-//document.getElementById("dog-btn3").addEventListener("click", addNewDoggo);
